cc0d7a486554206f9b0514073ca144b823b98861
Dat <82311930+dtdat0194@users.noreply.github.com>
2024-11-07 15:19:08 -0500

    Fixed model upload process / used memory

--- testMolmo.py
+++ testMolmo.py
@@ -1,45 +1,52 @@
 from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
 from PIL import Image
 import requests
+import torch
+import time
 
-# load the processor
-processor = AutoProcessor.from_pretrained(
-    'allenai/Molmo-7B-O-0924',
-    trust_remote_code=True,
-    torch_dtype='auto',
-    device_map='auto'
-)
-print('PROCESSOR')
-
-# load the model
-model = AutoModelForCausalLM.from_pretrained(
-    'allenai/Molmo-7B-O-0924',
-    trust_remote_code=True,
-    # torch_dtype=torch.bfloat16,
-    device_map='auto',
-    cache_dir='/fs/class-projects/fall2024/cmsc473/c473g000/hf_cache',
+with torch.no_grad():
+    # load the processor
+    processor = AutoProcessor.from_pretrained(
+        'allenai/Molmo-7B-O-0924',
+        trust_remote_code=True,
+        torch_dtype="auto",
+        device_map='auto',
+        cache_dir='/fs/class-projects/fall2024/cmsc473/c473g000/hf_cache',
     )
-print('MODEL')
+    print('PROCESSOR')
+    # print(processor.dtype)
+    # load the model
+    model = AutoModelForCausalLM.from_pretrained(
+        'allenai/Molmo-7B-O-0924',
+        trust_remote_code=True,
+        torch_dtype="auto",
+        device_map='auto',
+        cache_dir='/fs/class-projects/fall2024/cmsc473/c473g000/hf_cache',
+        )
+    print('MODEL')
+    print(model.dtype)
 
-# process the image and text
-inputs = processor.process(
-    images=[Image.open("Dog.jpg")],
-    text="Describe this image."
-)
+    start = time.time()
+    # process the image and text
+    inputs = processor.process(
+        images=[Image.open(requests.get("https://picsum.photos/id/237/536/354", stream=True).raw)],
+        text="Describe this image."
+    )
 
-# move inputs to the correct device and make a batch of size 1
-inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
+    # move inputs to the correct device and make a batch of size 1
+    inputs = {k: v.to(device=model.device).unsqueeze(0) for k, v in inputs.items()}
 
-# generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
-output = model.generate_from_batch(
-    inputs,
-    GenerationConfig(max_new_tokens=200, stop_strings="<|endoftext|>"),
-    tokenizer=processor.tokenizer
-)
+    # generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
+    output = model.generate_from_batch(
+        inputs,
+        GenerationConfig(max_new_tokens=200, stop_strings="<|endoftext|>"),
+        tokenizer=processor.tokenizer
+    )
 
-# only get generated tokens; decode them to text
-generated_tokens = output[0,inputs['input_ids'].size(1):]
-generated_text = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+    # only get generated tokens; decode them to text
+    generated_tokens = output[0,inputs['input_ids'].size(1):]
+    generated_text = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
 
-# print the generated text
-print(generated_text)
+    # print the generated text
+    print(generated_text)
+    print(time.time()-start)

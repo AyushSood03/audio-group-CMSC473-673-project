34dabeef6e8d0ea6330ad998e7ab47e2c80f758a
halo054 <139146470+halo054@users.noreply.github.com>
2024-12-13 17:53:16 -0500

    Update Caption_to_json.py

--- dataset/utils/Caption_to_json.py
+++ dataset/utils/Caption_to_json.py
@@ -1,13 +1,23 @@
 import json
+#This file changes a txt caption file (following the format mentioned in readme), into a json file, to be fed into evaluation metrices.
+
+
+#Change filename accordingly
 f = open("Audioset_GAMA_captions.txt","r")
 existing_caption = f.readlines()
+
+#Caption_dict make sure no duplicate data exist.
 caption_dict = {}
 index = 0
 while index < len(existing_caption):
-    caption_dict[existing_caption[index][:-1]] = existing_caption[index+1][:-1]
-    index+=3
+    caption_dict[existing_caption[index][:-1]] = existing_caption[index+1][:-1] #last char is '\n'.
+    index+=3 #Follows the format of txt file, one data point takes three lines
 f.close()
-print(len(caption_dict))
+
+#print(len(caption_dict))
+
+
+#Change the dict into a json file that can be put into evaluation metrics
 json_list = []
 for image_id in caption_dict.keys():
     temp = {}
